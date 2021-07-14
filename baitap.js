84f056bc87a82ccf21576d3f83a957cf39dcaff3
Nhung <nhungnth@relipasoft.com>
2021-07-14 18:25:32 +0700

    bai tap tinh lai suat

--- baitap.js
+++ baitap.js
@@ -16,3 +16,13 @@ function tinh() {
     let dt = r * r *pi
     document.write("Đường kính là: " + dk + "<br\>" + "Chu vi đường tròn là: " + cv + "<br\>" + "Diện tích hình tròn là: " + dt );
 }
+
+// bai tap tinh lai suat ngan hang
+function laisuat() {
+    let so_tien_vay = prompt("Hãy nhập số tiền vay: ");
+    let lai_suat = prompt("Hãy nhập lãi suất:");
+    let nam = prompt("Hãy nhập số năm:");
+    let so_tien_lai_don = so_tien_vay * lai_suat/100;
+    let so_tien_phai_tra = parseFloat(so_tien_vay) + so_tien_lai_don * nam;
+    document.write("Số tiền lãi phải trả là: " + so_tien_phai_tra);
+}
