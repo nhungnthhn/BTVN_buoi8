4086ff5b1b7fc6cba1db5e4f87f2d9cf5a957ba1
Nhung <nhungnth@relipasoft.com>
2021-07-14 18:10:35 +0700

    bai tap tinh chu vi va dien tich

--- baitap.js
+++ baitap.js
@@ -5,3 +5,14 @@ function date() {
     let dd = prompt ("Hãy nhập ngày");
     document.write("Thời gian là: " + dd + "/" + mm + "/" + yyyy)
 }
+
+// tinh chu vi, dien tich hinh tron
+
+const pi = 3.14
+function tinh() {
+    let r = prompt ("Hãy nhập bán kính:");
+    let dk = 2 *r;
+    let cv = 2 * pi * r
+    let dt = r * r *pi
+    document.write("Đường kính là: " + dk + "<br\>" + "Chu vi đường tròn là: " + cv + "<br\>" + "Diện tích hình tròn là: " + dt );
+}
