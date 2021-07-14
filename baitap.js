1e0fa7c5b45002c192f047f30b72278857ae2171
Nhung <nhungnth@relipasoft.com>
2021-07-14 18:02:20 +0700

    bai tạp

---
+++ baitap.js
@@ -0,0 +1,7 @@
+// hiển thị ngày tháng năm
+function date() {
+    let yyyy = prompt("Hãy nhập năm");
+    let mm = prompt ("Hãy nhập tháng:");
+    let dd = prompt ("Hãy nhập ngày");
+    document.write("Thời gian là: " + dd + "/" + mm + "/" + yyyy)
+}
